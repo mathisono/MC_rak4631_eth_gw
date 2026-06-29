8d235375badfd0fd1cef46110ebb2364e843eacf
mathison <mathisono@gmail.com>
2026-06-29 07:31:20 -0700

    Add Ethernet pending-client handoff state

--- meshcore_overlay/src/helpers/nrf52/EthernetSerialInterface.h
+++ meshcore_overlay/src/helpers/nrf52/EthernetSerialInterface.h
@@ -28,6 +28,78 @@
 #define ETH_START_DELAY_MS 0UL
 #endif
 
+#ifndef ETH_PENDING_FRAME_TIMEOUT_MS
+#define ETH_PENDING_FRAME_TIMEOUT_MS 3000UL
+#endif
+
+#ifndef ETH_PENDING_IDLE_TIMEOUT_MS
+#define ETH_PENDING_IDLE_TIMEOUT_MS 5000UL
+#endif
+
+#ifndef ETH_USE_STATIC_IP
+#define ETH_USE_STATIC_IP 0
+#endif
+
+#ifndef ETH_IP_A
+#define ETH_IP_A 192
+#endif
+#ifndef ETH_IP_B
+#define ETH_IP_B 168
+#endif
+#ifndef ETH_IP_C
+#define ETH_IP_C 3
+#endif
+#ifndef ETH_IP_D
+#define ETH_IP_D 55
+#endif
+
+#ifndef ETH_GATEWAY_A
+#define ETH_GATEWAY_A 192
+#endif
+#ifndef ETH_GATEWAY_B
+#define ETH_GATEWAY_B 168
+#endif
+#ifndef ETH_GATEWAY_C
+#define ETH_GATEWAY_C 3
+#endif
+#ifndef ETH_GATEWAY_D
+#define ETH_GATEWAY_D 3
+#endif
+
+#ifndef ETH_DNS_A
+#define ETH_DNS_A 192
+#endif
+#ifndef ETH_DNS_B
+#define ETH_DNS_B 168
+#endif
+#ifndef ETH_DNS_C
+#define ETH_DNS_C 3
+#endif
+#ifndef ETH_DNS_D
+#define ETH_DNS_D 3
+#endif
+
+#ifndef ETH_SUBNET_A
+#define ETH_SUBNET_A 255
+#endif
+#ifndef ETH_SUBNET_B
+#define ETH_SUBNET_B 255
+#endif
+#ifndef ETH_SUBNET_C
+#define ETH_SUBNET_C 255
+#endif
+#ifndef ETH_SUBNET_D
+#define ETH_SUBNET_D 0
+#endif
+
+#ifndef ETH_DISABLE_MAINTAIN
+#define ETH_DISABLE_MAINTAIN 0
+#endif
+
+#ifndef ETH_CHECK_LINK_STATUS
+#define ETH_CHECK_LINK_STATUS 1
+#endif
+
 #ifndef ETH_SPI_SCK
 #define ETH_SPI_SCK 3
 #endif
@@ -57,6 +129,13 @@
 #endif
 
 class EthernetSerialInterface : public BaseSerialInterface {
+  enum PendingRxState : uint8_t {
+    PENDING_WAIT_MAGIC = 0,
+    PENDING_READ_LEN_0,
+    PENDING_READ_LEN_1,
+    PENDING_READ_PAYLOAD
+  };
+
   bool deviceConnected;
   bool ethernetReady;
   bool ethernetStarted;
@@ -65,9 +144,11 @@ class EthernetSerialInterface : public BaseSerialInterface {
   unsigned long beginMillis;
   unsigned long lastDhcpAttempt;
   unsigned long lastMaintain;
+  unsigned long lastDelayLog;
 
   EthernetServer *server;
   mutable EthernetClient client;
+  mutable EthernetClient pendingClient;
 
   struct FrameHeader {
     uint8_t type;
@@ -86,9 +167,25 @@ class EthernetSerialInterface : public BaseSerialInterface {
   int send_queue_len;
   Frame send_queue[ETH_FRAME_QUEUE_SIZE];
 
+  uint8_t injected_frame[MAX_FRAME_SIZE];
+  uint16_t injected_frame_len;
+  bool injected_frame_valid;
+
+  uint8_t pending_frame_buf[MAX_FRAME_SIZE];
+  uint16_t pending_frame_len;
+  uint16_t pending_frame_pos;
+  PendingRxState pending_state;
+  unsigned long pending_last_progress_ms;
+
   void clearBuffers();
   void resetReceivedFrameHeader();
   bool hasReceivedFrameHeader() const;
+  void resetInjectedFrame();
+  void resetPendingParserOnly();
+  void resetPendingClient();
+  bool servicePendingClient();
+  bool promotePendingClientWithInjectedFrame();
+  bool promotePendingClientWithoutInjectedFrame();
   void makeMac(uint8_t mac[6]);
   void powerUpEthernet();
   void resetEthernet();
@@ -96,6 +193,7 @@ class EthernetSerialInterface : public BaseSerialInterface {
   void serviceEthernet();
   void serviceClient();
   void dropClient();
+  void ensureServerStarted();
 
 public:
   EthernetSerialInterface();
